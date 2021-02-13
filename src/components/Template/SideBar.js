e55511f7c61645d2eef7bd4e8c3739cf8518d598
S A Saharukh <sasaharukh@gmail.com>
2021-02-13 18:21:28 +0600

    edit side bar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -38,7 +38,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; S A Saharukh <Link to="/">saharukh.com</Link>.</p>
     </section>
   </section>
 );
